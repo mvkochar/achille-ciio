15813533e138b34306c1cbd204572a1c213b08e0
sgkochar <sgkochar@gmail.com>
2024-05-30 14:26:42 +0300

    add About, Latest and Looking blocks to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,102 @@ import './css/Home.css'
 const Home = () => {
   return (
     <>
-        <main className='home-main'>
-            <div className="home-main-above">Building what's next in the heart of Utah</div>
-            <h1 className="home-main-title">Find your favorite place</h1>
-        </main>
+      <main className='home-main'>
+        <div className="home-main-above">Building what's next in the heart of Utah</div>
+        <h1 className="home-main-title">Find your favorite place</h1>
+        <form action="" className='home-main-fm d-f'>
+          <select name="community">
+            <option value="0">Community</option>
+          </select>
+          <select name="city">
+            <option value="0">City</option>
+          </select>
+          <select name="type">
+            <option value="0">Type</option>
+          </select>
+          <button type='button'><img src="/images/search.png" alt="search" /></button>
+        </form>
+      </main>
+      <section className='home-about'>
+        <div className="d-f jc-sb align-center">
+          <div className='d-f'>
+            <div className='home-about-num'>(01)</div>
+            <div className="home-about-above">About us</div>
+          </div>
+          <div className="about-link-wr d-f">
+            <div className="about-link-title">Get to know us</div>
+            <a href="" className="about-link"><img src="/images/arrow-right.png" alt="arrow-right" /></a>
+          </div>
+        </div>
+        <h2 className="home-about-title">AChille Ciio is leading the way in creating unique real estate for Utah residents.</h2>
+      </section>
+      <section className='home-latest'>
+        <div className="d-f">
+          <div className="home-latest-num">(02)</div>
+          <div className="home-latest-above">Latest Projects</div>
+          <div className="home-latest-about">Featured Communities <br /> Ciio. Properties </div>
+        </div>
+        <div className="home-latest-media"><img src="/images/home-latest.png" alt="home-latest" /></div>
+        <div className="home-latest-info d-f">
+          <div>
+            <div>Affordable Units Starting</div>
+            <div className="home-latest-price">From $186,000</div>
+          </div>
+          <div>555 North 400 West <br /> Centerville, Utah 84014</div>
+          <div>50 Modern Townhomes <br /> Now Selling</div>
+        </div>
+      </section>
+      <section className='home-looking'>
+        <div className="d-f">
+          <div className="home-looking-num">(03)</div>
+          <div className="home-looking-above">I'm looking for:</div>
+        </div>
+        <div className="home-looking-caption d-f jc-sb align-center">
+          <h2 className="home-looking-title">Apartments</h2>
+          <button className='home-looking-btn'><img src="/images/arrow-down.png" alt="arrow-down" /></button>
+        </div>
+        <div className="home-looking-box d-f jc-sb">
+          <div className="home-looking-item">
+            <div><img src="/images/apartment1.png" alt="apartment1" /></div>
+            <div className="looking-item-info d-f">
+              <div>
+                <div className="info-title">theBEVERLY</div>
+                <p className="info-desc"> Salt Lake City</p>
+              </div>
+              <div>
+                <div className="info-title">Now Leasing</div>
+                <p className="info-desc">Apartments / Starting From — $1,675+ Monthly</p>
+              </div>
+            </div>
+          </div>
+          <div className="home-looking-item">
+            <div><img src="/images/apartment2.png" alt="apartment2" /></div>
+            <div className="looking-item-info d-f">
+              <div>
+                <div className="info-title">theBEVERLY</div>
+                <p className="info-desc"> Salt Lake City</p>
+              </div>
+              <div>
+                <div className="info-title">Now Leasing</div>
+                <p className="info-desc">Apartments / Starting From — $1,675+ Monthly</p>
+              </div>
+            </div>
+          </div>
+          <div className="home-looking-item">
+            <div><img src="/images/apartment2.png" alt="apartment2" /></div>
+            <div className="looking-item-info d-f">
+              <div>
+                <div className="info-title">theBEVERLY</div>
+                <p className="info-desc"> Salt Lake City</p>
+              </div>
+              <div>
+                <div className="info-title">Now Leasing</div>
+                <p className="info-desc">Apartments / Starting From — $1,675+ Monthly</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
