c1d06e6dc76e4cf102b09cad34ccb1ade061d23e
sgkochar <sgkochar@gmail.com>
2024-06-04 13:16:34 +0300

    add Team and Community blocks to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/About.css'
+import Connect from '../components/Connect/Connect'
 
 const About = () => {
     return (
@@ -65,6 +66,117 @@ const About = () => {
                 </div>
                 <div><img src="/images/about-advantages.png" alt="about-advantages" /></div>
             </section>
+            <section className='about-team'>
+                <div className="d-f">
+                    <div className="about-num">(03)</div>
+                    <h2 className="about-team-title">Process Makes <br /> perfect</h2>
+                </div>
+                <p className="about-team-desc">
+                    Get to know the executive team, the head honchos of the Achille Ciio
+                    pack—dedicated to finding you the ideal abode to call home. (We'd give
+                    it even more, but unfortunately, that's beyond mathematical possibility.)
+                </p>
+                <div className="about-team-founder">
+                    <div><img src="/images/team/1.png" alt="team1" /></div>
+                    <div className="team-member-name">Salim Ahmed</div>
+                    <div className="team-member-pos">Founder of Achile Ciio</div>
+                </div>
+                <div className="about-team-box d-f jc-sb">
+                    <div>
+                        <div><img src="/images/team/2.png" alt="team2" /></div>
+                        <div className="team-member-name">Kazi Mahbub</div>
+                        <div className="team-member-pos">CEO of Achille Ciio</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/3.png" alt="team3" /></div>
+                        <div className="team-member-name">Fazlor Rahman</div>
+                        <div className="team-member-pos">Chief Financial Office</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/4.png" alt="team4" /></div>
+                        <div className="team-member-name">Abdullah</div>
+                        <div className="team-member-pos">Chief Legal Officer</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/5.png" alt="team5" /></div>
+                        <div className="team-member-name">Samim Ahmed</div>
+                        <div className="team-member-pos">VP of Design & Architecture</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/6.png" alt="team6" /></div>
+                        <div className="team-member-name">Maruf Ahmed</div>
+                        <div className="team-member-pos">VP of Operations</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/7.png" alt="team7" /></div>
+                        <div className="team-member-name">Dildar Khan</div>
+                        <div className="team-member-pos">VP of Commercial Operations</div>
+                    </div>
+                </div>
+                <div className="d-f" style={{ marginTop: "200px" }}>
+                    <div className="about-num">(04)</div>
+                    <h2 className="about-team-title">Team Member</h2>
+                </div>
+                <p className="about-team-desc" style={{ width: "750px" }}>
+                    Our executive team couldn't do their magic without
+                    the creativity and support of our talented directors and
+                    their support crew.
+                </p>
+                <div className="about-team-box d-f jc-sb">
+                    <div>
+                        <div><img src="/images/team/8.png" alt="team8" /></div>
+                        <div className="team-member-name">Foysal Kabir</div>
+                        <div className="team-member-pos">Director of Construction</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/9.png" alt="team9" /></div>
+                        <div className="team-member-name">Akash Ahmed</div>
+                        <div className="team-member-pos">Director of Construction</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/10.png" alt="team10" /></div>
+                        <div className="team-member-name">Akash Ahmed</div>
+                        <div className="team-member-pos">Director of Land</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/11.png" alt="team11" /></div>
+                        <div className="team-member-name">Foysal Kabir</div>
+                        <div className="team-member-pos">Director of Land</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/11.png" alt="team11" /></div>
+                        <div className="team-member-name">Foysal Kabir</div>
+                        <div className="team-member-pos">Director of Land</div>
+                    </div>
+                    <div>
+                        <div><img src="/images/team/12.png" alt="team12" /></div>
+                        <div className="team-member-name">Akash Ahmed</div>
+                        <div className="team-member-pos">Director of Construction</div>
+                    </div>
+                </div>
+            </section>
+            <section className='about-community'>
+                <div className="d-f">
+                    <div className="about-num">(05)</div>
+                    <h2 className="about-community-title">We Build <br /> Community</h2>        
+                </div>
+                <div className="about-community-info">
+                        <div><img src="/images/about-community.png" alt="about-community" /></div>
+                        <p className="about-community-desc">
+                            Ciio is a real estate development and brokerage organization committed to developing
+                            and marketing master-planned communities, residential lots, urban in-fill communities,
+                            and mixed-use properties across the state of Utah. Our foundation is built on the beliefs
+                            of generating value through prudent land planning, innovative architecture, and outstanding
+                            execution at the project level.
+                        </p>
+                        <a href="" className="about-community-more">Learn More About Our Projects</a>
+                    </div>
+                    <div className="about-community-download d-f">
+                        <div className="dounload-title">Download Portfolio</div>
+                        <a href="" className="download-link"><img src="/images/arrow-right.png" alt="arrow-right" /></a>
+                    </div>
+            </section>
+            <Connect/>
         </>
     )
 }
